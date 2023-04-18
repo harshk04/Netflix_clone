7d5622241b16ebb710fd35374d657b5bee24b194
harshk04 <kumawatharsh2004@gmail.com>
2023-04-18 23:33:30 +0530

    commit5

---
+++ mail.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyADP0Ss3p6fHsQgXq6hbRgbDgDtbx1886A",
+  authDomain: "netflix-clone-61142.firebaseapp.com",
+  databaseURL: "https://netflix-clone-61142-default-rtdb.firebaseio.com",
+  projectId: "netflix-clone-61142",
+  storageBucket: "netflix-clone-61142.appspot.com",
+  messagingSenderId: "787572728881",
+  appId: "1:787572728881:web:1535ca6e74f6bce7292110",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var contactFormDB = firebase.database().ref("contactForm");
